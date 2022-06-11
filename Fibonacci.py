9aaffc546501bfeeaaaa6ea0beb97978d00b23f4
navneetujjain <84269021+navneetujjain@users.noreply.github.com>
2022-06-12 01:25:08 +0530

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,8 +1,6 @@
-#n=int(input())
+n=int(input())
 a=[0,1]
-a.append(-1)
-print(a)
-#for i in range(2,n+1):
-    #b=a[i-1]+a[i-2]
-    #a.append(b)
-#print(a[n-1])
+for i in range(2,n+1):
+    b=a[i-1]+a[i-2]
+    a.append(b)
+print(a[n-1])
