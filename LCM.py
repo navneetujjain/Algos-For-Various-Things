ae745d114ef966a478310fa97c20cc96eaa6708d
Navneet Ujjain <navneetujjain27@gmail.com>
2022-10-01 17:23:25 +0530

    Update LCM.py

--- LCM.py
+++ LCM.py
@@ -1,6 +1,23 @@
-#import math
+import math
 import numpy as np
+def gcd(a,b):
+  if int(a)>int(b):
+    n=int(a)
+    m=int(b)
+  else:
+    n=int(b)
+    m=int(a)
+  while m!=0:
+    k=m
+    m=n%m
+    n=k
+  return n
+
 a,b=input().strip().split(" ")
-#print(math.lcm(int(a),int(b)))
+# 1. A Lot Simple One Liner Numpy Method
 c=np.lcm(int(a),int(b))
 print(c)
+# 2. Little Mathematical Method
+d = gcd(a,b)
+e = (a * b) // d\
+print(e)
